bed584f9c6640b9238bede264516510420fee652
H-LeeVN <157027947+H-LeeVN@users.noreply.github.com>
2024-01-18 20:31:09 +0700

    Add files via upload

---
+++ obf_Z6mV83Dm5Zlz2b23pG6X0zKyAqW6XYM6428J2oK049XLEJC7xoKISKTtndq5E6ci.lua
@@ -0,0 +1,5 @@
+---// Join MTien Hub Sever To Update New Script \\--
+--// Sever Discord: https://discord.com/invite/pCBDH5z382 \\--
+_G.Name = "H-LeeVN" -- Put You Name Script Here
+_G.Logo = "https://www.roblox.com/library/16024483612/H-LeeVN" -- Enter Your ID Image Here
+loadstring(Game:HttpGet"https://raw.githubusercontent.com/ImTienNguyenZ/NguyenTienSimpRose/main/NguyenMinhTienLoader")()
